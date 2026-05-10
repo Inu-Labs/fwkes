b7cb918c2a862f28f25b7b911392bf38a82bc214
inunix3 <inunix3@tutamail.com>
2026-05-10 04:29:43 +0200

    Document joypad.h

--- include/fwkes/joypad.h
+++ include/fwkes/joypad.h
@@ -18,17 +18,41 @@
 
 #pragma once
 
+/**
+ * @file joypad.h
+ * @brief Standard NES controller.
+ *
+ * Original NES controllers were using internally CD4021 shift registers, so
+ * basically, we're emulating behavior of the shift register.
+ */
+
 #include <stdbool.h>
 #include <stdint.h>
 
+/**
+ * @def CONTROLLER_POLL_ADDR
+ * @brief Readable address common for both joypads.
+ */
 #define CONTROLLER_POLL_ADDR 0x4016
+
+/**
+ * @def CONTROLLER1_ADDR
+ * @brief Writable address of the primary joypad.
+ */
 #define CONTROLLER1_ADDR 0x4016
+/**
+ * @def CONTROLLER2_ADDR
+ * @brief Writable address of the secondary joypad.
+ */
 #define CONTROLLER2_ADDR 0x4017
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * @brief %Button bit masks.
+ */
 typedef enum {
     BTN_A = 1 << 0,
     BTN_B = 1 << 1,
@@ -40,6 +64,10 @@ typedef enum {
     BTN_RIGHT = 1 << 7
 } Button;
 
+/**
+ * @brief State of emulated joypad. In fact, it's representation of a CD4021
+ * shift register.
+ */
 typedef struct Joypad {
 #ifdef BUILD_RP2350
     uint8_t data;
@@ -50,9 +78,52 @@ typedef struct Joypad {
     bool strobe;
 } Joypad;
 
+/**
+ * @brief Reset state of joypad.
+ *
+ * All fields are set to 0.
+ *
+ * @param self Joypad instance.
+ */
 void joypad_reset(Joypad *self);
+/**
+ * @brief MMIO write interface.
+ *
+ * The first bit sets the strobe mode. When its 1, shift register is reloaded
+ * and consequent reads will return only staet of the first button. When its 0,
+ * shift register returns state of the current buttons and shift to the next
+ * one.
+ *
+ * Remaining 7 bits have no meaning and are ignored.
+ *
+ * @param self Joypad instance.
+ * @param value Strobe mode toggle.
+ */
 void joypad_write(Joypad *self, uint8_t data);
+/**
+ * @brief MMIO read interface with side effects.
+ *
+ * Every reading returns state of the current button. When strobe mode is
+ * enabled (see @ref joypad_write()), only the first button is read. If strobe
+ * mode is disabled, each read will return state of the current button and move
+ * to the next button (**side-effect**). When 8 buttons are read, it returns 1
+ * until it's reloaded by enabling strobe mode.
+ *
+ * @param self Joypad instance.
+ * @return State of the current button.
+ */
 uint8_t joypad_read(Joypad *self);
+/**
+ * @brief MMIO read interface without side effects.
+ *
+ * Every reading returns state of the current button. When strobe mode is
+ * enabled (see @ref joypad_write()), only the first button is read. If strobe
+ * mode is disabled, each read will return state of the current button. When 8
+ * buttons are read, it returns 1 until it's reloaded by enabling strobe mode.
+ *
+ * @param self Joypad instance.
+ * @return State of the current button.
+ */
 uint8_t joypad_peek(const Joypad *self);
 
 #ifdef __cplusplus
